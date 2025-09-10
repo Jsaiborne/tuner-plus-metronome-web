b26e1e35e59936494ef9951c47066e680213eaef
Jo <bahehdowski@gmail.com>
2025-09-11 03:37:26 +0530

    feat: add 404 page

---
+++ app/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+      </main>
+    );
+  }
+  
